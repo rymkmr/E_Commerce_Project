24531b42694cfe9144b261466c3b48502fbfec4b
rymkmr <rymkhoumeri@gmail.com>
2026-04-29 14:57:08 +0100

    Add database connection setup in db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$DB_HOST = "localhost";
+$DB_USER = "root";
+$DB_PASS = "";
+$DB_NAME = "ecommerce_project";
+
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
